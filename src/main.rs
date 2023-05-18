708dae766d5e99b78931c9b63771fae23f395431
sinku <sininoir@gmail.com>
2023-05-18 23:54:54 +0330

    Add binary conversion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's get encoding!");
+    println!("enter the phrase that you want to embed.");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("AAAAAAAA");
+
+    input = input.trim().to_string();
+
+    let mut input_in_binary = "".to_string();
+
+    for character in input.clone().into_bytes() {
+        input_in_binary += &format!("0{:b}", character);
+    }
+    println!("\"{}\" in binary is {}", input, input_in_binary);
+
 }
